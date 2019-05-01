906f373cdf4b8b3e02760111e6d2086a473a9c6c
Nichole Henry <nichole.henry12@gmail.com>
2019-05-01 00:27:03 -0500

    2nd commit

---
+++ CoderGirl_MVCMovies/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoderGirl_MVCMovies.Controllers
+{
+    public class MovieController : Controller
+    {
+        //private static <int, string> movies = new Dictionary<int, string>();
+         private static int nextIdToUse = 1;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string movies)
+        {
+            //movies.Add(nextIdToUse, movies);
+            nextIdToUse++;
+            return RedirectToAction(actionName: nameof(Index));
+
+        }
+
+
+
+    }
+}
