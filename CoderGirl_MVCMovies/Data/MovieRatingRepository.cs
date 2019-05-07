60bfd79613d0b76e3152237c2c324e52ef059eef
Nichole Henry <nichole.henry12@gmail.com>
2019-05-07 18:46:26 -0500

    models

---
+++ CoderGirl_MVCMovies/Data/MovieRatingRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoderGirl_MVCMovies.Models;
+
+namespace CoderGirl_MVCMovies.Data
+{
+    public class MovieRatingRepository : IMovieRatingRepository
+    {
+        static List<MovieRating> movieRating = new List<MovieRating>();
+        static int nextId = 1;
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MovieRating GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MovieRating> GetMovieRatings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Save(MovieRating movieRating)
+        {
+            
+        }
+
+    }
+}
