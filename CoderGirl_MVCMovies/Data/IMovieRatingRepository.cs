8f2295489f4bf477565265edb6454babe0fe670b
Nichole Henry <nichole.henry12@gmail.com>
2019-05-31 11:47:44 -0500

    commit

--- CoderGirl_MVCMovies/Data/IMovieRatingRepository.cs
+++ CoderGirl_MVCMovies/Data/IMovieRatingRepository.cs
@@ -1,25 +1,59 @@
-﻿using CoderGirl_MVCMovies.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
+using CoderGirl_MVCMovies.Models;
 
 namespace CoderGirl_MVCMovies.Data
 {
+    // TODO: Implement this interface
     public interface IMovieRatingRepository
-    {
-        int Save(MovieRating movieRating);
+    {        
+        /// <summary>
+        /// Given a movieName and rating, saves the rating and returns a unique id > 0.
+        /// If the movie name and/or rating are null or empty, nothing should be saved and it should return 0
+        /// </summary>
+        /// <param name="movieName"></param>
+        /// <param name="rating"></param>
+        /// <returns></returns>
+        int SaveRating(string movieName, int rating);
 
-        List<MovieRating> GetMovieRatings();
-
+        /// <summary>
+        /// Given an id, will return the associated rating 
+        /// If the id does not exist, returns 0
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        int GetRatingById(int id);
+        void Save(MovieRating movieRating);
         MovieRating GetById(int id);
 
-        void Update(MovieRating movieRating);
+        // double GetAverageRatingByMovieName(string movieName);
 
+        /// <summary>
+        /// Given an id, will return the associated movie name.
+        /// If the id does not exist, return null
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        string GetMovieNameById(int id);
+        void Update(MovieRating movieRating);
         void Delete(int id);
 
-        double GetAverageRating(int movieId);
+        /// <summary>
+        /// Given a movie name, returns the average rating of of the movie.
+        /// If there are no ratings for the movie, returns an empty list.
+        /// </summary>
+        /// <param name="movieName"></param>
+        /// <returns></returns>
+        double GetAverageRatingByMovieName(string movieName);
 
-        int GetRatingCount(int movieId);
+        /// <summary>
+        /// Returns a list of all the ids of saved movie ratings
+        /// </summary>
+        /// <returns></returns>
+        List<int> GetIds();
+        List<MovieRating> GetMovieRatings();
     }
 }
